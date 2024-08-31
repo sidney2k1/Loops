b4fb286bce94c82a78b76683c543513534328a30
sidney2k1 <sidsingla02@gmail.com>
2024-08-31 14:20:12 +0530

    main2

---
+++ main2.py
@@ -0,0 +1,15 @@
+#input number greater than 1
+n=int(input("Enter a number"))
+#print the number from n to 1
+print("the numbers from {0} to {1} are:".format(n,1))
+#loop to print numbers
+for i in range(n,0,-1):
+    print(i)
+
+#input number greater than 1
+n=int(input("Enter a number"))
+#print the number from n to 1
+print("the numbers from {0} to {1} are:".format(n,1))
+#loop to print numbers
+for i in range(n,1,-1):
+    print(i)
